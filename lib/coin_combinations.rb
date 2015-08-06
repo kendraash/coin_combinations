25fc65d099ad64b467f98b580869dc9d47fa352b
Julianna Green & Kendra Ash <student@epicodus.com>
2015-08-06 13:53:29 -0700

    Adding simplifying by using % instead of dividing

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -5,21 +5,21 @@ class Fixnum
 
     if change < 99 && change >= 25
       change_quarter = change./(25)
-      change = change.-(change_quarter.*(25))
+      change = change.%(25)
       quarter = change_quarter.to_s().concat(" quarter")
       final_change.push(quarter)
     end
 
     if change < 25 && change >= 10
       change_dimes = change./(10)
-      change = change.-(change_dimes.*(10))
+      change = change.%(10)
       dimes = change_dimes.to_s().concat(" dime")
       final_change.push(dimes)
     end
 
     if change < 10 && change >= 5
       change_nickels = change./(5)
-      change = change.-(change_nickels.*(5))
+      change = change.%(5)
       nickels = change_nickels.to_s().concat(" nickel")
       final_change.push(nickels)
     end
