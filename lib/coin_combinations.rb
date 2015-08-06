a221c419b36f3fa5bb71871d0a4c6ddab804af66
Julianna Green & Kendra Ash <student@epicodus.com>
2015-08-06 11:37:28 -0700

    Passing spec for returning dimes in change

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -2,16 +2,25 @@ class Fixnum
   define_method(:coin_combinations) do
     change = self
     final_change = []
-  if change < 10 && change >= 5
-    change_nickels = change./(5)
-    change = change.-(change_nickels.*(5)) 
-    nickels = change_nickels.to_s().concat(" nickel and")
-    final_change.push(nickels)
-  end
-  if change < 5
-    pennies = change.to_s().concat(" pennies")
-    final_change.push(pennies)
+
+    if change < 25 && change >= 10
+      change_dimes = change./(10)
+      change = change.-(change_dimes.*(10))
+      dimes = change_dimes.to_s().concat(" dime")
+      final_change.push(dimes)
     end
-    final_change.join(" ")
+
+    if change < 10 && change >= 5
+      change_nickels = change./(5)
+      change = change.-(change_nickels.*(5))
+      nickels = change_nickels.to_s().concat(" nickel")
+      final_change.push(nickels)
+    end
+
+    if change < 5 && change > 0
+      pennies = change.to_s().concat(" pennies")
+      final_change.push(pennies)
+    end
+      final_change.join(" and ")
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -8,5 +8,9 @@ describe('Fixnum#coin_combinations') do
   it("it takes an amount of money less than ten and return change with least amount of coins") do
     expect(9.coin_combinations()).to(eq("1 nickel and 4 pennies"))
   end
-
-end
+  it ("it takes an amount of money less than 25 and returns the change with the least amount of coins") do
+    expect(15.coin_combinations()).to(eq("1 dime and 1 nickel"))
+  end
+  it ("takes an amount of money less than 50 and returns the change with the least amount of coins") do
+    expect(47.coin_)
+  end
