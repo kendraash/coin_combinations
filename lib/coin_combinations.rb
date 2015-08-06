681e055a435f5781dbbfd8fcade40097451990ff
Julianna Green & Kendra Ash <student@epicodus.com>
2015-08-06 10:42:20 -0700

    First spec which takes an amount less than five and returns the number of pennies to give

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,7 +1,10 @@
-# class Fixnum
-#   define_method(:coin_combinations) do
-#     if self < 5
-#       self.tostring()
-#       self.concat(" pennies")
-#   end
-# end
+class Fixnum
+  define_method(:coin_combinations) do
+    change = []
+    if self < 5
+    change = self.to_s()
+    change.concat(" pennies")
+    end
+    change
+  end
+end
