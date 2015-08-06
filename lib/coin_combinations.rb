8d698b71f2f76bcc93d60babce0eee2705bf83ac
Julianna Green & Kendra Ash <student@epicodus.com>
2015-08-06 11:22:48 -0700

    Passing second spec which returns nickels

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,10 +1,17 @@
 class Fixnum
   define_method(:coin_combinations) do
-    change = []
-    if self < 5
-    change = self.to_s()
-    change.concat(" pennies")
+    change = self
+    final_change = []
+  if change < 10 && change >= 5
+    change_nickels = change./(5)
+    change = change.-(change_nickels.*(5)) 
+    nickels = change_nickels.to_s().concat(" nickel and")
+    final_change.push(nickels)
+  end
+  if change < 5
+    pennies = change.to_s().concat(" pennies")
+    final_change.push(pennies)
     end
-    change
+    final_change.join(" ")
   end
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -2,7 +2,11 @@ require('rspec')
 require('coin_combinations')
 
 describe('Fixnum#coin_combinations') do
-  it("it takes an amount of money and return change with least amount of coins") do
+  it("it takes an amount of money less than five pennies and return change with least amount of coins") do
     expect(4.coin_combinations()).to(eq("4 pennies"))
   end
+  it("it takes an amount of money less than ten and return change with least amount of coins") do
+    expect(9.coin_combinations()).to(eq("1 nickel and 4 pennies"))
+  end
+
 end
